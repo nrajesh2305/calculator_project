4fc6eaf5a0fefe11f3dcf5eb5cd39d03941435cb
Nithin Rajesh <rajeshnithin2305@csu.fullerton.edu>
2023-12-27 14:18:47 -0800

    Finished creating the numbers, and operator. Finished an operator and negate function.

--- script.js
+++ script.js
@@ -22,3 +22,31 @@ function divide(num1, num2)
     return num1 / num2;
 }
 
+let firstNum = document.querySelector("#first_number");
+let operator = document.querySelector("#operator");
+let secondNum = document.querySelector("#second_number");
+
+function operate(operator, num1, num2)
+{
+    if(operator == "+")
+    {
+        add(num1, num2);
+    }
+    else if(operator == "-")
+    {
+        subtract(num1, num2);
+    }
+    else if(operator == "*")
+    {
+        multiply(num1, num2);
+    }
+    else
+    {
+        divide(num1, num2);
+    }
+}
+
+function negate(num)
+{
+    return num * -1;
+}
